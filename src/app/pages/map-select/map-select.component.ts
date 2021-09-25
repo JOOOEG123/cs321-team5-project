4280664760f0fa9a85e61e4928d268726f97ebe5
Thomas <clarkeyt624@gmail.com>
2021-09-25 19:27:31 -0400

    firebase functionality added to mapselect

--- src/app/pages/map-select/map-select.component.ts
+++ src/app/pages/map-select/map-select.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DndMap } from '@core/models/auth/map.model';
+import { FirebaseService } from '@core/services/firebase/firebase.service';
+import { DB_CONFIG } from 'src/environments/environment';
 
 @Component({
   selector: 'app-map-select',
@@ -10,18 +12,26 @@ export class MapSelectComponent implements OnInit {
 
   maps = [] as DndMap[];
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService
+  ) { }
 
   ngOnInit(): void {
     this.getMaps();
   }
 
   getMaps() {
-    const map = new DndMap();
-    map.name = 'Test Map';
-    map.description = 'This map is a test';
-    map.imageUrl = 'https://i.redd.it/pq61m18mmzp51.jpg';
-    this.maps.push(map)
+    this.firebaseService.getEntries(DB_CONFIG.map_endpoint, 'modifiedDate', true).subscribe((maps: DndMap[]) => {
+      if(maps) {
+        this.maps = maps;
+      } else {
+        const map = new DndMap();
+        map.name = 'Test Map';
+        map.description = 'This map is a test';
+        map.imageUrl = 'https://i.redd.it/pq61m18mmzp51.jpg';
+        this.maps.push(map);
+      }
+    });
   }
 
   clicked(map: DndMap) {
@@ -30,6 +40,11 @@ export class MapSelectComponent implements OnInit {
 
   newMap() {
     console.log('new map clicked');
+    this.firebaseService.saveEntry(this.maps[0], DB_CONFIG.map_endpoint).then(id => {
+      this.maps[0].id = id;
+    }).catch(err => {
+      throw new err;
+    })
   }
 
 }
