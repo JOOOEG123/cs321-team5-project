c40426be9b1f17361c2d51f52948bba24a1a4a10
rsbbrs <rsb102.rs@gmail.com>
2021-10-10 13:13:26 -0400

    Finished the sidebar
    Changed the look of some buttons since I couldn't get it to work properly otherwise. Also, copied the code from the user-profile page to get stuff from firebase.

--- src/app/pages/user-nav-bar/user-nav-bar.component.ts
+++ src/app/pages/user-nav-bar/user-nav-bar.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestoreDocument } from '@angular/fire/firestore';
+import { UserProfile } from '@core/models/auth/auth.model';
+import { DataStorageService } from '@core/services/data-storage/dataStorage.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-nav-bar',
   templateUrl: './user-nav-bar.component.html',
   styleUrls: ['./user-nav-bar.component.scss']
 })
+
+// Copied what was in the UserProfileComponent to get the name from the database.
 export class UserNavBarComponent {
+  private _document!: AngularFirestoreDocument<UserProfile>;
+  uid: string | undefined;
+  profileDetails!: Observable<UserProfile | undefined>;
 
-  url="";
+  constructor(
+    private dataStorage: DataStorageService,
+    private auth: AngularFireAuth
+  ) {}
 
-  constructor(private router: Router) {
-    this.url = this.router.url;
+  async ngOnInit() {
+    this.uid = (await this.auth.currentUser)?.uid;
+    if (this.uid !== undefined) {
+      this._document = this.dataStorage.getProfileDetails(this.uid);
+      this.profileDetails = this._document.valueChanges();
+    }
   }
 
 }
