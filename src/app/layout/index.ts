9520f628ed5dad380a68b0e67899912bdfa107c8
JOOOEG123 <57308580+JOOOEG123@users.noreply.github.com>
2021-10-23 12:43:51 -0400

    remove left nav button

--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -1,12 +1,10 @@
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
-import { LeftNavBtnComponent } from './left-nav-btn/left-nav-btn.component';
 import { UserNavBarComponent } from './user-nav-bar/user-nav-bar.component';
 export const AllLayoutComponents = [
   FooterComponent,
   HeaderComponent,
   LayoutComponent,
-  LeftNavBtnComponent,
   UserNavBarComponent,
 ];
