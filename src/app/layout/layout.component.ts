71b8a45ff3148bafdb6491b6a7e3f507e8cb6159
JOOOEG123 <57308580+JOOOEG123@users.noreply.github.com>
2021-10-23 13:38:36 -0400

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `<app-header></app-header>
-    <app-left-nav-btn></app-left-nav-btn>
     <app-user-nav-bar></app-user-nav-bar>
     <router-outlet></router-outlet>
     <app-footer></app-footer>`,
