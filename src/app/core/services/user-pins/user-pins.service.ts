42ac0f0270a3c9bbb1202430a215090a07fb1c04
JOOOEG123 <57308580+JOOOEG123@users.noreply.github.com>
2021-10-28 22:49:27 -0400

    Update user-pins.service.ts

--- src/app/core/services/user-pins/user-pins.service.ts
+++ src/app/core/services/user-pins/user-pins.service.ts
@@ -8,86 +8,86 @@ import { DataStorageService } from '../data-storage/dataStorage.service';
   providedIn: 'root',
 })
 export class UserPinsService {
-  // private _userPin = new BehaviorSubject<Pin[]>([]);
-  // readonly userPin = this._userPin.asObservable();
+  private _userPin = new BehaviorSubject<Pin[]>([]);
+  readonly userPin = this._userPin.asObservable();
 
-  // constructor(
-  //   private auth: AngularFireAuth,
-  //   private dataStorage: DataStorageService
-  // ) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private dataStorage: DataStorageService
+  ) {}
 
-  // getObservedUserPins(): Promise<any> {
-  //   return this.userPin.toPromise();
-  // }
+  getObservedUserPins(): Promise<any> {
+    return this.userPin.toPromise();
+  }
 
-  // async updateUserMap(
-  //   pins: Pin[],
-  //   mapId: string | undefined,
-  //   index?: number | undefined
-  // ): Promise<any> {
-  //   const user = await this.auth.currentUser;
-  //   if (!mapId) throw new Error('[Error] - map id was not passed.');
-  //   try {
-  //     const allUserPin = await this.getAllUserPins(mapId);
-  //     if (allUserPin?.user_maps && user) {
-  //       const { user_pins } = allUserPin;
-  //       if (index && user_pins) {
-  //         user_pins[index] = pins;
-  //       } else if (user_pins) {
-  //         user_pins.push(pins);
-  //       }
-  //       this._userPin.next(user_pins);
-  //       return this.dataStorage.addUserPins(pins, user.uid, mapId);
-  //     }
-  //     this._userPin.next(pins);
-  //     return this.dataStorage.addUserPins(pins, user?.uid, mapId);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
+  async updateUserMap(
+    pins: Pin[],
+    mapId: string | undefined,
+    index?: number | undefined
+  ): Promise<any> {
+    const user = await this.auth.currentUser;
+    if (!mapId) throw new Error('[Error] - map id was not passed.');
+    try {
+      const allUserPin = await this.getAllUserPins(mapId);
+      if (allUserPin?.user_maps && user) {
+        const { user_pins } = allUserPin;
+        if (index && user_pins) {
+          user_pins[index] = pins;
+        } else if (user_pins) {
+          user_pins.push(pins);
+        }
+        this._userPin.next(user_pins);
+        return this.dataStorage.addUserPins(pins, user.uid, mapId);
+      }
+      this._userPin.next(pins);
+      return this.dataStorage.addUserPins(pins, user?.uid, mapId);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  // async deleteUserMap(index: number, mapId: string): Promise<any> {
-  //   const user = await this.auth.currentUser;
-  //   try {
-  //     const allUserPin = await this.getAllUserPins(mapId);
-  //     if (allUserPin) {
-  //       const { user_pins } = allUserPin;
-  //       if (user_pins.length !== 0) {
-  //         user_pins.splice(index, 1);
-  //         this._userPin.next(user_pins);
-  //         return this.dataStorage.addUserPins(user_pins, user?.uid, mapId);
-  //       }
-  //       this._userPin.next([]);
-  //       return this.dataStorage.addUserPins([], user?.uid, mapId);
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //     return null;
-  //   }
-  // }
+  async deleteUserMap(index: number, mapId: string): Promise<any> {
+    const user = await this.auth.currentUser;
+    try {
+      const allUserPin = await this.getAllUserPins(mapId);
+      if (allUserPin) {
+        const { user_pins } = allUserPin;
+        if (user_pins.length !== 0) {
+          user_pins.splice(index, 1);
+          this._userPin.next(user_pins);
+          return this.dataStorage.addUserPins(user_pins, user?.uid, mapId);
+        }
+        this._userPin.next([]);
+        return this.dataStorage.addUserPins([], user?.uid, mapId);
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
-  // async getAllUserPins(mapId: string) {
-  //   const user = await this.auth.currentUser;
-  //   const data = (
-  //     await this.dataStorage.getUserPin(user?.uid, mapId).toPromise()
-  //   ).data();
-  //   this._userPin.next(data?.user_pins || []);
-  //   return data;
-  // }
+  async getAllUserPins(mapId: string) {
+    const user = await this.auth.currentUser;
+    const data = (
+      await this.dataStorage.getUserPin(user?.uid, mapId).toPromise()
+    ).data();
+    this._userPin.next(data?.user_pins || []);
+    return data;
+  }
 
-  // async getUserMapByIndex(index: number | undefined, mapId: string) {
-  //   try {
-  //     const allUserPin = await this.getAllUserPins(mapId);
-  //     if (allUserPin) {
-  //       const { user_pins } = allUserPin;
-  //       if (user_pins.length !== 0 && index) {
-  //         return user_pins[index];
-  //       }
-  //     }
-  //     return {};
-  //   } catch (error) {
-  //     console.error(error);
-  //     return null;
-  //   }
-  // }
+  async getUserMapByIndex(index: number | undefined, mapId: string) {
+    try {
+      const allUserPin = await this.getAllUserPins(mapId);
+      if (allUserPin) {
+        const { user_pins } = allUserPin;
+        if (user_pins.length !== 0 && index) {
+          return user_pins[index];
+        }
+      }
+      return {};
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 }
