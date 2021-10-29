8dfea353e7858e889f80ecc36ae07178a031e9c1
rsbbrs <rsb102.rs@gmail.com>
2021-10-29 12:53:56 -0400

    Finished form
    The form now displays the current account information, and allows the user to edit and save their changes. Changes are saved on the database. There are only two issues. The first is that even though the email changes, only the original email can be used to sign in. Second is that I don't know how to change password.

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -4,7 +4,6 @@ import { AngularFirestoreDocument } from '@angular/fire/firestore';
 import { UserProfile } from '@core/models/auth/auth.model';
 import { DataStorageService } from '@core/services/data-storage/dataStorage.service';
 import { Observable } from 'rxjs';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -33,18 +32,7 @@ export class SettingsComponent implements OnInit {
   constructor(
     private dataStorage: DataStorageService,
     private auth: AngularFireAuth,
-    private formBuilder: FormBuilder,
   ) {}
-
-  //Form used to get data from the user as input.
-  accountData = this.formBuilder.group({
-    username: '',
-    email: '',
-    firstName: '',
-    lastName: '',
-    country: '',
-    birthday: '',
-  });
 
   async ngOnInit() {
     this.uid = (await this.auth.currentUser)?.uid;
@@ -62,7 +50,6 @@ export class SettingsComponent implements OnInit {
 
   //Runs updateProfileDetails with the new data after the user clicks submit.
   onSubmit(): void {
-
     this.dataStorage.updateProfileDetails(this.userProfile);
   }
 
