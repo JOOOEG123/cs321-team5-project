358b8b489e23502251fd788affc8364b58686c87
SGseth-Cmd <78049978+SGseth-Cmd@users.noreply.github.com>
2021-11-06 11:25:06 -0400

    homepage changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { environment } from './../environments/environment';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgxBootstrapModule } from '@core/modules/ngx-bootstrap/ngx-bootstrap.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AllLayoutComponents } from './layout';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AllLayoutComponents } from './layout';
+import { environment } from './../environments/environment';
 import { PagesComponent } from './pages';
-import { CommonModule } from '@angular/common';
-import { NgxBootstrapModule } from '@core/modules/ngx-bootstrap/ngx-bootstrap.module';
-import { AuthErrorDisplayComponent } from './shared/auth-error-display/auth-error-display.component';
 import { SharedComponent } from './shared';
-import { MapViewComponent } from './pages/map-view/map-view.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-
-
 
 @NgModule({
   declarations: [
@@ -26,6 +24,7 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
     ...AllLayoutComponents,
     ...PagesComponent,
     ...SharedComponent,
+
   ],
   imports: [
     AngularFireAuthModule,
@@ -34,14 +33,16 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
     AngularFirestoreModule,
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     NgxBootstrapModule,
-    CarouselModule.forRoot()
+    NgxSpinnerModule,
+    ReactiveFormsModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
